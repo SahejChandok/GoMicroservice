d3b133c4b6bc0f61de2825dc89b11b970d2da05f
SahejChandok <chandoksahej@gmail.com>
2024-12-26 11:51:37 -0500

    handlers, creating ervers, logging, graceful shutdown

--- main.go
+++ main.go
@@ -1,26 +1,35 @@
 package main
 
 import (
-	"fmt"
-	"io"
+	"context"
+	"log"
+	"microservicesGo/handlers"
 	"net/http"
+	"os"
+	"time"
 )
 
 func main() {
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("hello World")
-		d, err := io.ReadAll(r.Body)
-		if err!= nil{
-			http.Error(w, "oops", http.StatusBadRequest)
-			return 
-		}
-		fmt.Fprintf(w, "Hello %s", d)
-	})
-	http.HandleFunc("/GoodBye", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("Goodbye")
-	})
+	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+	hh := handlers.NewHello(l)
+	gh := handlers.NewGoodbye(l)
+	sm := http.NewServeMux()
 
-	http.ListenAndServe(":9090", nil)
-	//shaej
+	sm.Handle("/", hh)
+	sm.Handle("/bye", gh)
+
+	s := &http.Server{
+		Addr:         ":9090",
+		Handler:      sm,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+		IdleTimeout:  120 * time.Second,
+	}
+
+	s.ListenAndServe()
+
+	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(tc)
+
 }
